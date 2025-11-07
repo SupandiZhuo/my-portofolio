adf4abb8c09fca3d4ce46ea9f92f338d8570bb92
SupandiZhuo <161045662+SupandiZhuo@users.noreply.github.com>
2025-11-07 22:27:22 +0700

    Update portfolio: hosting through gh-page

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
   plugins: [react()],
-  base: './',  // ← Change to this instead of '/my-portofolio/'
+  base: '/my-portofolio/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false
+    emptyOutDir: true
   }
 })
